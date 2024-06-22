dc1ecc65525e93061795546c8040e09c0559ac71
Hariom Sharma <108718496+hariomsharmacse@users.noreply.github.com>
2024-06-22 13:26:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,51 +1,78 @@
 const button = document.getElementById('btn');
 const inputBox = document.getElementById('input_text');
-const outputBoxes = document.querySelectorAll('.boxes');
+const box = document.querySelector('.boxes');
 const deleteBtn = document.querySelectorAll('.delete_btn');
+let localstorageValues = JSON.parse(localStorage.getItem("todoList")) || [];
 
 
 button.addEventListener('click', () => {
-    let newDiv = document.createElement('div');
-    let newBtn = document.createElement('button');
-    let btnTrack = [];
- 
+  let value = inputBox.value.trim();
+  localstorageValues.push(value);
+  
+  localStorage.setItem("todoList", JSON.stringify(localstorageValues));
+  
+  // console.log(localstorageValues);
+  
+  let newDiv = document.createElement('div');
+  let newBtn = document.createElement('button');
+
+        newBtn.classList.add('delete_btn');
+        newBtn.className = "delete_btn";
+        newBtn.innerText = "❌";
+        // console.log(inputBox.value);
+        newDiv.classList.add('box');
+        newDiv.innerText = inputBox.value;
+        inputBox.value = "";
     
-    // newBtn.classList.add('delete_btn');
-    newBtn.className = "delete_btn";
-    newBtn.innerText = "❌";
-    // console.log(inputBox.value);
-    newDiv.classList.add('box');
-    newDiv.innerText = inputBox.value;
-    outputBoxes.forEach((box) => {
         box.appendChild(newDiv);
         newDiv.appendChild(newBtn);
-    })
-    
-  let allBtn = document.querySelectorAll('button');
 
-  allBtn.forEach((btn, index) => {
-    btn.id = "btn"+(index+1)
-    btnTrack.push(btn.id);
-  })
-
-  newBtn.addEventListener('click', () => {
-    console.log(newBtn.id);
-    console.log(btnTrack)
-    if(!btnTrack.includes(btn.id)){
-        console.log('yes it is')
-
-        outputBoxes.forEach((box) => {
-            box.removeChild(newDiv);
-            newDiv.removeChild(newBtn);
-        })
-
-    }
-  })
-
-
-   
+        newBtn.addEventListener('click', (e) => {
+          removeBtn(e);
+      })
+      
    
 })
+
+// console.log(localstorageValues);
+
+function setAllvalues() {
+  localstorageValues.forEach(element => {
+    let newDiv = document.createElement('div');
+    let newBtn = document.createElement('button');
+
+    newBtn.className = "delete_btn";
+        newBtn.innerText = "❌";
+        // console.log(inputBox.value);
+        newDiv.classList.add('box');
+        newDiv.innerText = element;
+        inputBox.value = "";
+    
+        box.appendChild(newDiv);
+        newDiv.appendChild(newBtn);
+        
+        newBtn.addEventListener('click', (event) => {
+        removeBtn(event);
+      })
+
+    });
+}
+
+setAllvalues();
+
+function removeBtn(data) {
+  let currTarget = data.target;
+  let previousValue = currTarget.parentNode;
+  box.removeChild(previousValue);
+  let dataofPre = (previousValue.innerText).slice(0, -1);
+
+  let indexofPre = localstorageValues.indexOf(dataofPre);
+  // console.log(dataofPre);
+  localstorageValues.splice(indexofPre, 1);
+  // console.log(localstorageValues);
+  localStorage.setItem("todoList", JSON.stringify(localstorageValues));
+}
+
 
 
 
